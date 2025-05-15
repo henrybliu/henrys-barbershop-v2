4745fddf48d9d3e661cdeb8297cdf3af926bf8d3
Henry Liu <11.henrybliu@gmail.com>
2025-05-15 16:59:19 -0700

    initial setup

---
+++ eslint.config.js
@@ -0,0 +1,64 @@
+import js from "@eslint/js";
+import eslintPluginImport from "eslint-plugin-import";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      import: eslintPluginImport,
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "external",
+              position: "before",
+            },
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  }
+);
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Calendar from "./components/calendar/calendar";
+import Gallery from "./components/gallery/gallery";
+import Home from "./components/home/home";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Calendar />
+      <Gallery />
+    </>
+  );
+}
+
+export default App;
